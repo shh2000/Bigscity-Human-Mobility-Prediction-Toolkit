aa99f6f4fb3343ba22f47e307224450f36548ffd
MarcAntoineAlex <18510756258@163.com>
2020-10-24 12:28:11 +0800

    修改test-hstlstm

--- test/test-HST-LSTM.py
+++ test/test-HST-LSTM.py
@@ -8,21 +8,23 @@ from runner.run_hst_lstm import HSTLSTMRunner
 
 with open("../global_config_sample.json") as f:
     path = json.load(f)
-    dir_path = path['path']
+    dir_path = path['server_path']
 
 with open(os.path.join(dir_path, "config/run/hst-lstm.json")) as f:
     config = json.load(f)
 config["device"] = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+config["dir_path"] = dir_path
 
 pre = torch.randint(0, config["aoi_size"], [config['batch_size']*4, config['session_size'], config['step_size'], 3]).long()
 pre[:, :, :, 1] = torch.rand(config['batch_size']*4, config['session_size'], config['step_size']) * config["temporal_slot_size"]
 pre[:, :, :, 2] = torch.rand(config['batch_size']*4, config['session_size'], config['step_size']) * config["spacial_slot_size"]
 
-model = HST_LSTM.HSTLSTM(dir_path, config)
-runner = HSTLSTMRunner(dir_path, config)
-runner.train(model, pre)
+runner = HSTLSTMRunner(config, dir_path)
+runner.init_model(config)
+runner.train(pre)
 
-torch.save(model.cpu(), './trained_model.pkl')
+# torch.save(model.cpu(), './trained_model.pkl')
+runner.save_cache("trained_model.pkl")
 np.save("pre.npy", pre.numpy())
 
-print(runner.predict(model, pre))
+print(runner.predict(pre))
