3729939027704c7326e5dbaee5b8c950a525b63c
WenMellors <33283819+WenMellors@users.noreply.github.com>
2020-11-01 16:00:16 +0800

    Jwj adjust deepmove (#12)
    * use fousquare.pk
    
    * delete batch
    
    * fix target_len
    
    * fix target_len
    
    * fix bug
    
    * use cache
    
    * adjust parameters
    
    * use foursquare-tky
    
    * fix pre
    
    * add batch again
    
    * fix model
    
    * restrict history len
    
    * fix predict

--- presentation/gen_history_pre.py
+++ presentation/gen_history_pre.py
@@ -35,23 +35,34 @@ class GenHistoryPre(Presentation):
         def collactor(batch):
             loc = []
             tim = []
+            loc_len = [] # 用于 pack_padded_sequence
             history_loc = []
             history_tim = []
-            history_count = []
+            history_len = [] # 用于 pack_padded_sequnce
             uid = []
             target = []
             session_id = []
             for item in batch:
                 loc.append(item['loc'])
                 tim.append(item['tim'])
+                loc_len.append(len(item['loc']))
                 history_loc.append(item['history_loc'])
                 history_tim.append(item['history_tim'])
-                history_count.append(item['history_count'])
+                history_len.append(len(item['history_loc']))
                 uid.append(item['uid'])
                 target.append(item['target'])
                 session_id.append(item['session_id'])
-            return loc, tim, history_loc, history_tim, history_count, uid, target, session_id
-        data_loader = DataLoader(dataset=dataset, batch_size=self.config['batch_size'], num_workers=self.config['num_workers'], collate_fn=collactor)
+            max_loc_len = max(loc_len)
+            max_history_len = max(history_len)
+            for i in range(len(loc)):
+                if len(loc[i]) < max_loc_len:
+                    loc[i] += [self.pad_item[0]] * (max_loc_len - len(loc[i]))
+                    tim[i] += [self.pad_item[1]] * (max_loc_len - len(tim[i]))
+                if len(history_loc[i]) < max_history_len:
+                    history_loc[i] += [self.pad_item[0]] * (max_history_len - len(history_loc[i]))
+                    history_tim[i] += [self.pad_item[1]] * (max_history_len - len(history_tim[i]))
+            return loc, tim, loc_len, history_loc, history_tim, history_len, uid, target, session_id
+        data_loader = DataLoader(dataset=dataset, batch_size=self.config['batch_size'], num_workers=self.config['num_workers'], collate_fn=collactor, shuffle=True)
         total_batch = dataset.__len__() / self.config['batch_size']
         return {'loader': data_loader, 'total_batch': total_batch}
 
@@ -59,8 +70,7 @@ class GenHistoryPre(Presentation):
         res = {
             'loc_size': self.data['loc_size'],
             'tim_size': self.data['tim_size'],
-            'uid_size': self.data['uid_size'],
-            'target_len': self.config['pad_len'] - self.config['history_len']
+            'uid_size': self.data['uid_size']
         }
         return res
 
@@ -213,11 +223,9 @@ class GenHistoryPre(Presentation):
         '''
         pad_item: (loc_pad, tim_pad)
         return list of data
-        (loc, tim, history_loc, hisory_tim, history_count, uid, target)
         '''
         data = []
         user_set = self.data['data_neural'].keys()
-        pad_len = self.config['pad_len']
         history_len = self.config['history_len']
         for u in user_set:
             sessions = self.data['data_neural'][u]['sessions']
@@ -225,67 +233,41 @@ class GenHistoryPre(Presentation):
                 train_id = self.data['data_neural'][u]['train'] + self.data['data_neural'][u]['eval'] + self.data['data_neural'][u]['test']
             else:
                 train_id = self.data['data_neural'][u][mode]
-            for c, i in enumerate(train_id):
+            for c, session_id in enumerate(train_id):
                 trace = {}
                 if mode == 'train' and c == 0 or mode == 'all' and c == 0:
                     continue
-                session = sessions[i]
+                session = sessions[session_id]
                 if len(session) <= 1:
                     continue
                 ## refactor target
-                target = [s[0] for s in session[1:]]
-                if len(target) < pad_len - history_len:
-                    pad_list = [self.pad_item[0] for i in range(pad_len - history_len - len(target))]
-                    target = target + pad_list
-                else:
-                    target = target[-(pad_len - history_len):]
+                target = session[-1][0]
                 history = []
-                if mode == 'test':
+                if mode == 'eval':
                     test_id = self.data['data_neural'][u]['train']
+                    for tt in test_id:
+                        history.extend([(s[0], s[1]) for s in sessions[tt]])
+                if mode == 'test':
+                    test_id = self.data['data_neural'][u]['train'] + self.data['data_neural'][u]['eval']
                     for tt in test_id:
                         history.extend([(s[0], s[1]) for s in sessions[tt]])
                 for j in range(c):
                     history.extend([(s[0], s[1]) for s in sessions[train_id[j]]])
-                # refactor history
-                if len(history) >= history_len:
-                    # 取后 history_len 个点
+                # history 太长了根本没法训练
+                if len(history) > history_len:
                     history = history[-history_len:]
-                else:
-                    # 将 history 填充足够
-                    pad_history = [self.pad_item for i in range(history_len - len(history))]
-                    history = pad_history + history
-                history_tim = [t[1] for t in history]
-                history_count = [1]
-                last_t = history_tim[0]
-                count = 1
-                for t in history_tim[1:]:
-                    if t == last_t:
-                        count += 1
-                    else:
-                        history_count[-1] = count
-                        history_count.append(1)
-                        last_t = t
-                        count = 1
                 history_loc = [s[0] for s in history]  # 把多个 history 路径合并成一个？
                 history_tim = [s[1] for s in history]
                 trace['history_loc'] = history_loc
                 trace['history_tim'] = history_tim
-                trace['history_count'] = history_count
-                loc_tim = history
+                loc_tim = []
                 loc_tim.extend([(s[0], s[1]) for s in session[:-1]])
-                # refactor loc tim
-                if len(loc_tim) < pad_len:
-                    pad_list = [self.pad_item for i in range(pad_len - len(loc_tim))]
-                    loc_tim = loc_tim + pad_list
-                else:
-                    # 截断
-                    loc_tim = loc_tim[-pad_len:]
                 loc_np = [s[0] for s in loc_tim]
                 tim_np = [s[1] for s in loc_tim]
                 trace['loc'] = loc_np # loc 会与 history loc 有重合， loc 的前半部分为 history loc
                 trace['tim'] = tim_np
                 trace['target'] = target  # target 会与 loc 有一段的重合，只有 target 的最后一位 loc 没有
                 trace['uid'] = int(u)
-                trace['session_id'] = i
+                trace['session_id'] = session_id
                 data.append(trace)
         return data
